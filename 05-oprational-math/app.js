8ebcfaa13ea1f682238d403d580b9de83938fc2b
kemplu007 <akimkemplu@gmail.com>
2018-11-29 01:59:19 -0800

    a

---
+++ 05-oprational-math/app.js
@@ -0,0 +1,27 @@
+const numb1 = 100;
+const numb2 = 50;
+let val;
+
+// simple math oprator
+val = numb1 +numb2;
+
+console.log(val);
+
+// math object
+val = Math.PI;
+val = Math.E;
+
+// pembulatan
+val = Math.round(3.4);
+val = Math.ceil(3.4); //dipaksa keatas
+val = Math.floor(3.8);//dipaksa ke lebih kecil
+val = Math.sqrt(149); //mencari akar kuadrat;
+val = Math.abs(-89); //memuat positif nilai;
+val = Math.pow(8, 10);//pemangkatan (x, y)
+val = Math.min(9, 9, 200, 100, 123, 4, 11, -2, 23, ); //mencari nilai terendah
+val = Math.max(9, 9, 200, 100, 123, 4, 11, -2, 23, ); //mencari nilai tertinggi
+val = Math.random() * 30;// mencetak angka acak
+
+
+
+console.log(val);

8d4b0100931ad818d6c370796b30c4de258bc8d5
kemplu007 <akimkemplu@gmail.com>
2018-11-30 07:26:51 -0800

    mp3

--- 11-gawe-mp3/app.js
+++ 11-gawe-mp3/app.js
@@ -1,14 +1,42 @@
 let musik = new Audio();
 musik.src= 'audio/18AndLife.mp3'
-musik.loop= false;
+musik.loop= true;
 musik.play();
+let geser;
  
 function mulaiAudio() {
     let play= document.getElementById('play');
-    let mute = document.getElementById('mute');
+    let mute=document.getElementById('mute');
+    let musikSlider = document.getElementById('musikSlider');
+    let volumeSlider = document.getElementById('volumeSlider');
+    let waktuSekarang = document.getElementById('waktuSekarang');
+    let durasi = document.getElementById('durasi');
+    
 
     play.addEventListener('click', fplay);
         mute.addEventListener('click', fmute);
+        volumeSlider.addEventListener('mousemove', setVolume);
+        musikSlider.addEventListener('mousedown', function(e){
+            geser =true;
+            setPosisiMusik(e);
+        });
+             musikSlider.addEventListener('mousemove', function(e) {
+                 setPosisiMusik(e);
+             });
+
+              musikSlider.addEventListener('mousemup', function() {
+                geser =false;
+            });
+            function setPosisiMusik(e){
+                if (geser){
+                    let posisiMusik = musik.duration* (musikSlider.value/100);
+                    musik.currentTime = posisiMusik;
+                }
+
+            }
+        function setVolume() {
+            musik.volume=volumeSlider.value / 100;
+        }
 
         function fplay() {
             if (musik.paused) {
@@ -27,6 +55,21 @@ function mulaiAudio() {
                 musik.muted = true
                 mute.style.background = "url(img/unmute.png";
             }
+        }
+        musik.addEventListener('timeupdate', function(){
+            updateWaktu();
+        })
+        function updateWaktu(){
+            let waktuBaru = musik.currentTime * (100/musik.duration);
+            musikSlider.value = waktuBaru;
+
+            let menitSekarang = Math.floor(musik.currentTime/60);
+            let detikSekarang = Math.floor(musik.currentTime - menitSekarang * 60);
+            let menitDurasi = Math.floor(musik.duration/60);
+            let detikDurasi = Math.floor(musik.duration -menitDurasi* 60);
+
+            waktuSekarang.innerHTML=menitSekarang + ':' + detikSekarang;
+            durasi.innerHTML=menitDurasi + ':' + detikDurasi;
         }
 
 }
